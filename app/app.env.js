7451d64bb6b961124f8e790435dee9c99ab45b61
Bernard Butaye <bernardenmartine@gmail.com>
2017-02-19 21:58:19 +0100

    update api location

--- app/app.env.js
+++ app/app.env.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'http://klusbibapi';
+  window.__env.apiUrl = 'http://api.klusbib.be';
 
   // Base url
   window.__env.baseUrl = '/';
