fe7439536e91a0db9306c55bc2cbc36da0cfbb73
Bernard Butaye <bernardenmartine@gmail.com>
2018-03-27 21:37:21 +0200

    rename volunteer calendar ctrl method

--- app/components/volunteer-calendar/volunteer-calendar.component.js
+++ app/components/volunteer-calendar/volunteer-calendar.component.js
@@ -2,7 +2,7 @@ angular.module('volunteerCalendar').component('volunteerCalendar', {
 	templateUrl : '/components/volunteer-calendar/volunteer-calendar.template.html',
 	controller :
 	[ '__env',
-		function ToolListController(__env) {
+		function VolunteerCalendarController(__env) {
 
 		var self = this;
 	} ]
