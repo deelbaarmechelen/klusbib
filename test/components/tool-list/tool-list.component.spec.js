4f307c62e5354c5cd5fc01a673d8b58c3a33712c
Bernard Butaye <bernardenmartine@gmail.com>
2018-04-08 18:05:42 +0200

    complete tool list tests

--- test/components/tool-list/tool-list.component.spec.js
+++ test/components/tool-list/tool-list.component.spec.js
@@ -41,32 +41,43 @@ describe('toolList', function() {
 
 	// Test the controller
 	describe('ToolListController', function() {
-        var $httpBackend, ctrl, $localStorage, User, UserService, __env_, stateParams;
+        var $httpBackend, ctrl, $localStorage, User, UserService, __env_, $stateParams;
 
         // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
         // This allows us to inject a service and assign it to a variable with the same name
         // as the service while avoiding a name conflict.
-        beforeEach(inject(function($componentController, _$httpBackend_, _$localStorage_, _User_, _UserService_, ___env_, $stateParams) {
+        beforeEach(inject(function($componentController, _$httpBackend_, _$localStorage_, _User_, _UserService_, ___env_, _$stateParams_) {
           $httpBackend = _$httpBackend_;
           $httpBackend.expectGET('data/tools/tools.json')
           .respond([{name: 'Cirkelzaag'}, {name: 'hamer'}]);
 
-          ctrl = $componentController('toolList');
+          ctrl = $componentController('toolList', null, {
+              // tools: 'tools',
+              category: 'construction'
+          });
           $localStorage = _$localStorage_;
           User = _User_;
           UserService = _UserService_;
           __env = ___env_;
-          stateParams = $stateParams;
+          $stateParams = _$stateParams_;
         }));
 
-	  xit('should create a `tools` property with 2 tools fetched with `$http`', function() {
-		  expect(ctrl.tools).toBeUndefined();
+        xit('should create a `tools` property with 2 tools fetched with `$http`', function() {
+            // Moved to router-ui -> how to test?
+            expect(ctrl.tools).toBeUndefined();
 
-		  $httpBackend.flush();
-		  expect(ctrl.tools).toEqual([{name: 'Cirkelzaag'}, {name: 'hamer'}]);
-	  });
+		    $httpBackend.flush();
+		    expect(ctrl.tools).toEqual([{name: 'Cirkelzaag'}, {name: 'hamer'}]);
+        });
 
-	  it('should translate category in human readable name', function() {
+        it('should define newTool method', function() {
+            expect(ctrl.newTool).toBeDefined();
+        });
+        it('should define deleteTool method', function() {
+            expect(ctrl.deleteTool).toBeDefined();
+        });
+
+	    it('should translate category in human readable name', function() {
           expect(ctrl.translateCategory).toBeDefined();
 		  expect(ctrl.translateCategory('wood')).toEqual('Schrijnwerk');
 		  expect(ctrl.translateCategory('technics')).toEqual('Technieken');
@@ -74,7 +85,7 @@ describe('toolList', function() {
           expect(ctrl.translateCategory('general')).toEqual('Algemeen');
 		  // when no translation available, keep category name
 		  expect(ctrl.translateCategory('unknown')).toEqual('unknown');
-	  });
+	    });
 	});
 
 });
